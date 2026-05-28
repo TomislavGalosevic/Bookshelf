53eb6b6170305b4aabcb6bed40d8fb5ec1054145
Tomislav Galosevic <tgalosevic@gmail.com>
2026-05-28 14:38:18 +0200

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,18 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: function(origin, callback) {
+    const allowed = [
+      'http://localhost:3000',
+      'https://bookshelf-eta-five.vercel.app',
+      process.env.CLIENT_URL
+    ];
+    if (!origin || allowed.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
